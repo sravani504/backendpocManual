277bb9870eba8a2937a625152aecfd4eefdc7e72
vamshi_gujjuboina_7113 <vamshi.gujjuboina@3insys.com>
2023-01-28 07:42:16 +0530

    Edit and View

--- routers/Post.js
+++ routers/Post.js
@@ -1,4 +1,4 @@
-import { chapterusers,getChapters,editChapters,pageUsers} from "../controllers/auth.js";
+import { chapterusers,getChapters,editChapters,pageUsers, editPages} from "../controllers/auth.js";
 
 import  express from "express";
 const router=express.Router();
@@ -6,5 +6,6 @@ const router=express.Router();
 router.post("/add", chapterusers )
 router.patch("/addpage/:id",pageUsers );
 router.get("/getchapters", getChapters)
-router.patch('/add/update/:id',editChapters)
+router.patch('/add/update/:id',editChapters);
+router.patch("/editPage/:id", editPages);
 export default router;
